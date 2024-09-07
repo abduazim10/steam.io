ed8721ec3140b7e1efaed61b803b117fb38987f8
abduazim10 <abduazimit@gmail.com>
2024-09-07 22:38:20 +0500

    yaxshi

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById('login_btn_signin').addEventListener('click', function() {
+    var audio = document.getElementById('audio');
+    audio.volume = 1.0; // 100% volume
+    audio.play();
+});
